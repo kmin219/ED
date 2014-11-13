69296d09e2b23c0f68ef7747c8135bbde918da28
kmin219 <kmin219@gmail.com>
2014-11-12 19:53:00 -0600

    files
    f

---
+++ KW/main_test_Metric.m
@@ -0,0 +1,23 @@
+clc;clear all;close all;
+% test_H2_1();
+test_H2_2();
+test_H2_3();
+test_H2_4();
+
+test_H3_1();
+test_H3_2();
+test_H3_3();
+test_H3_4();
+
+test_H1_1();
+% test_H3_2();
+% test_H3_3();
+% test_H3_4();
+
+test_H4_1();
+% test_H3_2();
+% test_H3_3();
+% test_H3_4();
+
+
+test_H2_1();
