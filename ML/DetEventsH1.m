d190b3fb7489e1dcd4d040daae86cc7cf0648e70
Min Lwin <m.lwin@utexas.edu>
2014-11-14 04:46:03 -0600

    Outline of Paper and H1Events

--- ML/DetEventsH1.m
+++ ML/DetEventsH1.m
@@ -6,59 +6,59 @@ clear path
 %% Load Distance Metrics 
 
 %  On Events: load distances in P
-load MetricP_all_on_H2_1.mat
-load MetricP_all_on_H2_2.mat
-load MetricP_all_on_H2_3.mat
-load MetricP_all_on_H2_4.mat
+load MetricP_all_on_H1_1.mat
+load MetricP_all_on_H1_2.mat
+load MetricP_all_on_H1_3.mat
+load MetricP_all_on_H1_4.mat
 
 %  On Events: load distances in Q
-load MetricQ_all_on_H2_1.mat
-load MetricQ_all_on_H2_2.mat
-load MetricQ_all_on_H2_3.mat
-load MetricQ_all_on_H2_4.mat
+load MetricQ_all_on_H1_1.mat
+load MetricQ_all_on_H1_2.mat
+load MetricQ_all_on_H1_3.mat
+load MetricQ_all_on_H1_4.mat
 
 %  Off Events: load distances in P
-load MetricP_all_off_H2_1.mat
-load MetricP_all_off_H2_2.mat
-load MetricP_all_off_H2_3.mat
-load MetricP_all_off_H2_4.mat
+load MetricP_all_off_H1_1.mat
+load MetricP_all_off_H1_2.mat
+load MetricP_all_off_H1_3.mat
+load MetricP_all_off_H1_4.mat
 
 %  Off Events: load distances in Q
-load MetricQ_all_off_H2_1.mat
-load MetricQ_all_off_H2_2.mat
-load MetricQ_all_off_H2_3.mat
-load MetricQ_all_off_H2_4.mat
+load MetricQ_all_off_H1_1.mat
+load MetricQ_all_off_H1_2.mat
+load MetricQ_all_off_H1_3.mat
+load MetricQ_all_off_H1_4.mat
 
 %  Load Windows to get thresh
-load window_P_H2_all
-load window_Q_H2_all
+load window_P_H1_all
+load window_Q_H1_all
 
 %% Detect On/Off Events for Test 1 Data
 
 % Detect On Events
 disp('Looking for On events in Test 1');
-metPdata = MetricP_all_on_H2_1;
-metQdata = MetricQ_all_on_H2_1;
-winP = window_P_H2_all;
-winQ = window_Q_H2_all;
+metPdata = MetricP_all_on_H1_1;
+metQdata = MetricQ_all_on_H1_1;
+winP = window_P_H1_all;
+winQ = window_Q_H1_all;
 
 onEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 % Detect Off Events
 disp('Looking for Off events in Test 1');
-metPdata = MetricP_all_off_H2_1;
-metQdata = MetricQ_all_off_H2_1;
+metPdata = MetricP_all_off_H1_1;
+metQdata = MetricQ_all_off_H1_1;
 
 offEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 %  Combine On/Off
-eventsH2_1 = onEvents;
+eventsH1_1 = onEvents;
 for i = 1:length(onEvents);
     for j = 1:length(onEvents{i});
         if any(onEvents{i}{j})
             firstOn = min(find(onEvents{i}{j}>0));
             offEvents{i}{j}(1:firstOn) = 0;
-            eventsH2_1{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
+            eventsH1_1{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
         end
     end
 end
@@ -67,28 +67,28 @@ end
 
 % Detect On Events
 disp('Looking for On events in Test 2');
-metPdata = MetricP_all_on_H2_2;
-metQdata = MetricQ_all_on_H2_2;
-winP = window_P_H2_all;
-winQ = window_Q_H2_all;
+metPdata = MetricP_all_on_H1_2;
+metQdata = MetricQ_all_on_H1_2;
+winP = window_P_H1_all;
+winQ = window_Q_H1_all;
 
 onEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 % Detect Off Events
 disp('Looking for Off events in Test 2');
-metPdata = MetricP_all_off_H2_2;
-metQdata = MetricQ_all_off_H2_2;
+metPdata = MetricP_all_off_H1_2;
+metQdata = MetricQ_all_off_H1_2;
 
 offEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 %  Combine On/Off
-eventsH2_2 = onEvents;
+eventsH1_2 = onEvents;
 for i = 1:length(onEvents);
     for j = 1:length(onEvents{i});
         if any(onEvents{i}{j})
             firstOn = min(find(onEvents{i}{j}>0));
             offEvents{i}{j}(1:firstOn) = 0;
-            eventsH2_2{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
+            eventsH1_2{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
         end
     end
 end
@@ -96,28 +96,28 @@ end
 
 % Detect On Events
 disp('Looking for On events in Test 3');
-metPdata = MetricP_all_on_H2_3;
-metQdata = MetricQ_all_on_H2_3;
-winP = window_P_H2_all;
-winQ = window_Q_H2_all;
+metPdata = MetricP_all_on_H1_3;
+metQdata = MetricQ_all_on_H1_3;
+winP = window_P_H1_all;
+winQ = window_Q_H1_all;
 
 onEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 % Detect Off Events
 disp('Looking for Off events in Test 3');
-metPdata = MetricP_all_off_H2_3;
-metQdata = MetricQ_all_off_H2_3;
+metPdata = MetricP_all_off_H1_3;
+metQdata = MetricQ_all_off_H1_3;
 
 offEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 %  Combine On/Off
-eventsH2_3 = onEvents;
+eventsH1_3 = onEvents;
 for i = 1:length(onEvents);
     for j = 1:length(onEvents{i});
         if any(onEvents{i}{j})
             firstOn = min(find(onEvents{i}{j}>0));
             offEvents{i}{j}(1:firstOn) = 0;
-            eventsH2_3{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
+            eventsH1_3{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
         end
     end
 end
@@ -126,38 +126,38 @@ end
 
 % Detect On Events
 disp('Looking for On events in Test 4');
-metPdata = MetricP_all_on_H2_4;
-metQdata = MetricQ_all_on_H2_4;
-winP = window_P_H2_all;
-winQ = window_Q_H2_all;
+metPdata = MetricP_all_on_H1_4;
+metQdata = MetricQ_all_on_H1_4;
+winP = window_P_H1_all;
+winQ = window_Q_H1_all;
 
 onEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 % Detect Off Events
 disp('Looking for Off events in Test 4');
-metPdata = MetricP_all_off_H2_4;
-metQdata = MetricQ_all_off_H2_4;
+metPdata = MetricP_all_off_H1_4;
+metQdata = MetricQ_all_off_H1_4;
 
 offEvents = FindEvents(metPdata, metQdata, winP, winQ);
 
 %  Combine On/Off
-eventsH2_4 = onEvents;
+eventsH1_4 = onEvents;
 for i = 1:length(onEvents);
     for j = 1:length(onEvents{i});
         if any(onEvents{i}{j})
             firstOn = min(find(onEvents{i}{j}>0));
             offEvents{i}{j}(1:firstOn) = 0;
-            eventsH2_4{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
+            eventsH1_4{i}{j} = onEvents{i}{j}+offEvents{i}{j}*-1;
         end
     end
 end
 
-save('events_H2.mat', 'eventsH2_1','eventsH2_2','eventsH2_3','eventsH2_4');
+save('events_H1.mat', 'eventsH1_1','eventsH1_2','eventsH1_3','eventsH1_4');
 
 %%  Visualize Combined On Off results to verify
 % 
-% metPdata = MetricP_all_on_H2_1;
-% metQdata = MetricQ_all_on_H2_1;
+% metPdata = MetricP_all_on_H1_1;
+% metQdata = MetricQ_all_on_H1_1;
 % 
 % for i = 1:numApp
 %     disp(i)
@@ -184,8 +184,8 @@ save('events_H2.mat', 'eventsH2_1','eventsH2_2','eventsH2_3','eventsH2_4');
 
 %%  Visualize Combined Off results to verify
 % 
-% metPdata = MetricP_all_off_H2_1;
-% metQdata = MetricQ_all_off_H2_1;
+% metPdata = MetricP_all_off_H1_1;
+% metQdata = MetricQ_all_off_H1_1;
 % 
 % for i = 1:numApp
 %     disp(i)
@@ -212,8 +212,8 @@ save('events_H2.mat', 'eventsH2_1','eventsH2_2','eventsH2_3','eventsH2_4');
 % 
 %%  Visualize Combined On results to verify
 % 
-% metPdata = MetricP_all_on_H2_1;
-% metQdata = MetricQ_all_on_H2_1;
+% metPdata = MetricP_all_on_H1_1;
+% metQdata = MetricQ_all_on_H1_1;
 % 
 % for i = 1:numApp
 %     disp(i)
